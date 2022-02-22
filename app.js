40f9676f9585b2cfdc52e3ca65b3710c7cbd08b4
Miyka96 <micaelaseminara@outlook.it>
2022-02-22 19:48:57 +0100

    js progress

--- app.js
+++ app.js
@@ -2,33 +2,32 @@ const computerNumberWrapper = document.querySelector('.computer-number-wrapper')
 let numberWrapper
 let number
 const playBtn = document.getElementById("play-btn");
-let numberGenerated
-let numberArray = [];
+
 
 for (let i = 0; i < 5; i++) {
-    let numberWrapper = document.createElement('div');
+    numberWrapper = document.createElement('div');
     numberWrapper.classList.add('number-wrapper');
-    let number = document.createElement('div');
+    number = document.createElement('div');
     number.classList.add('number');
     computerNumberWrapper.append(numberWrapper);
     numberWrapper.append(number);
 }
 
 playBtn.addEventListener('click', function(){
-    generateFiveNumbers(); 
+   numberElements= document.getElementsByClassName("number");
+   for (let i=0; i<5; i++){
+    numberElements[i].append(getRandomIntInclusive(1, 20));
+   }  
 })
 
+
+
+
+// FUNZIONI ------------------------------------
 function generateFiveNumbers (){
-    for (let i=0 ; i<5 ;i++){
-        let numberGenerated = getRandomIntInclusive(1, 20)   
-        console.log(numberGenerated)
-    }     
+        let random = getRandomIntInclusive(1, 20);
+        return (random)           
 }
-
-
-
-
-
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
